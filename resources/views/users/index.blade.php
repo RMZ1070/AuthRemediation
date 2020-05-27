3664357a068d4519bf9f374f4b7673fd094c8bd0
Rmz <xfa1070@hotmail.com>
2020-05-27 12:57:46 +0200

    user plus petit que 6 caractére

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -58,6 +58,28 @@
                     </ul>                   
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header">Users <a href="/user-create" class="btn btn-success ">New</a></div>
+
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($users as $item)
+                        @if (strlen($item)> 6)
+                            
+                        
+                    <li class="list-group-item">
+                        {{$item->name}}
+                        <a href="/user-edit/{{$item->id}}" class=" btn btn-warning">Edit</a>
+                        <form action="/user-delete/{{$item->id}}" method="post" class="d-inline">
+                            @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                        </li>
+                        @endif  
+                        @endforeach
+                    </ul>                   
+                </div>
+            </div>
 
         </div>
     </div>
